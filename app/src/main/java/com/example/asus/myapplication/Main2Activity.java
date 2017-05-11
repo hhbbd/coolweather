43e759b3af8b9fe890e64195418360f429d3b486
hhbb <hhbb>
2017-05-11 11:43:28 +0800

    回退按钮

--- app/src/main/java/com/example/asus/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/asus/myapplication/Main2Activity.java
@@ -7,28 +7,28 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class Main2Activity extends AppCompatActivity {
-    private String [] beijing={"asd","asd","dsa","sad","sd","as"};
-    private String [] zhejiang={"ds","sd"};
-    private String [] City={};
+//    private String [] beijing={"asd","asd","dsa","sad","sd","as"};
+//    private String [] zhejiang={"ds","sd"};
+//    private String [] City={};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Intent intent=getIntent();
-        String data=intent.getStringExtra("p");
-        String city[]=null;
-        if ("北京".equals(data)){
-            city=beijing;
-        }
-        else if ("浙江".equals(data)){
-            city=zhejiang;
-        }
-        else{
-            city=City;
-        }
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(
-                Main2Activity.this,android.R.layout.simple_list_item_1,city);
-        ListView listView =(ListView) findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
+//        Intent intent=getIntent();
+//        String data=intent.getStringExtra("p");
+//        String city[]=null;
+//        if ("北京".equals(data)){
+//            city=beijing;
+//        }
+//        else if ("浙江".equals(data)){
+//            city=zhejiang;
+//        }
+//        else{
+//            city=City;
+//        }
+//        ArrayAdapter<String> adapter=new ArrayAdapter<String>(
+//                Main2Activity.this,android.R.layout.simple_list_item_1,city);
+//        ListView listView =(ListView) findViewById(R.id.list_view);
+//        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/asus/myapplication/MainActivity.java
+++ app/src/main/java/com/example/asus/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,22 +16,26 @@ import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
     private String [] province=new String[]{"北京","上海","浙江"};
-    private String [] beijing=new String[]{"asd","asd","dsa","sad","sd","as"};
-    private String [] zhejiang=new String[]{"ds","sd"};
+    private String [] beijing=new String[]{"东城区","西城区","朝阳区","丰台区","石景山区","海淀区","门头沟区","房山区","通州区"
+            ,"顺义区","昌平区","大兴区","怀柔区","平谷区","密云区","延庆区"};
+    private String [] zhejiang=new String[]{"杭州","宁波","温州","绍兴","湖州","嘉兴","金华","衢州","舟山","台州","丽水"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ListView listView =(ListView) findViewById(R.id.list_view);
+        final Button backButton = (Button) findViewById(R.id.back_button);
+
 
         ArrayAdapter adapter=
         new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,province);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
             @Override
 
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                backButton.setVisibility(View.VISIBLE);
 
                 if ("北京".equals(province[position])) {
                     ArrayAdapter adapter =
@@ -50,6 +55,17 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
+        });
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                backButton.setVisibility(View.GONE);
+                ArrayAdapter adapter =
+                        new ArrayAdapter<String>(MainActivity.this
+                                , android.R.layout.simple_list_item_1,
+                                province);
+                listView.setAdapter(adapter);
+            }
         });
 
     }
